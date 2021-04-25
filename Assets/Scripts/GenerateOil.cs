c440a4bc7168552376b87ea86add4f7db465331c
domster704 <domster704@mail.ru>
2021-04-25 21:30:29 +0400

    new polygon

---
+++ Assets/Scripts/GenerateOil.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class GenerateOil : MonoBehaviour
+{
+
+    public GameObject dropOil;
+
+    void Start()
+    {
+        for (int i = 0; i < 2; i++) {
+            Instantiate(dropOil, dropOil.transform.position, dropOil.transform.rotation);
+        }
+    }
+
+}
---
+++ Assets/Scripts/TakeObject.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TakeObject : MonoBehaviour
+{
+
+    public Camera _cam;
+
+    static GameObject current_gameObj;
+
+    public GameObject test;
+
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+            rayC();
+        }
+
+        if (Input.GetButtonDown("Fire2")) {
+            putObj();
+        }
+    }
+
+    private void putObj() {
+        RaycastHit hit;
+
+        if (Physics.Raycast(_cam.transform.position, _cam.transform.forward, out hit)) {
+            // Instantiate(test, hit.point, hit.transform.rotation);
+            if (current_gameObj != null) {
+                // Instantiate(current_gameObj, hit.point, hit.transform.rotation);
+
+                current_gameObj.SetActive(true);
+                current_gameObj.transform.position = hit.point;
+                current_gameObj.transform.rotation = hit.transform.rotation;
+
+
+                current_gameObj = null;
+            }
+        }
+    }
+
+    void rayC() {
+        RaycastHit hit;
+
+        if (Physics.Raycast(_cam.transform.position, _cam.transform.forward, out hit)) {
+            if (hit.collider.name.Contains("Flask")) {
+                current_gameObj = hit.collider.gameObject;
+                // hit.collider.transform.SetParent(_cam.transform);
+                current_gameObj.SetActive(false);
+            }
+        }
+    }
+}
